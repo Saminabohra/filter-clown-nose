65a0debd6b329566a78652eff91ebf76c51b6f0b
Samina bohra <72374206+Saminabohra@users.noreply.github.com>
2022-01-14 19:19:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', getPose);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+fill("red");
+stroke("red");
+circle(7,7,20);
+
+}
+
+function take_snapshot(){
+    save("myFilterImage.png");
+}
+
+function modelLoaded(){
+    console.log("poseNet is loaded");
+}
+
+function getPose(result){
+    if(result.length > 0){
+        console.log(results);
+        console.log('nose x = '+ result[0].pose.nose.x);
+        console.log('nose y = '+ result[0].pose.nose.y);
+    }
+}
